d2f2964dd28b335dd0f5ca0f395b6a6df01e3312
Md Abdul Quyum <quyumbdse@gmail.com.com>
2024-02-06 11:57:19 +0100

    Handle error

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,19 +2,23 @@
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
   return (
-     <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color='red'>Delete Issue</Button>
-      </AlertDialog.Trigger>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color='red'>Delete Issue</Button>
+        </AlertDialog.Trigger>
         <AlertDialog.Content style={{ maxWidth: 500 }}>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
           <AlertDialog.Description size="2">
-              Are you sure? This application will no longer be accessible and any
-              existing sessions will be expired.
+            Are you sure? This application will no longer be accessible and any
+            existing sessions will be expired.
           </AlertDialog.Description>
 
           <Flex gap="3" mt="4" justify="end">
@@ -24,16 +28,35 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-            <Button variant="solid" color="red" onClick={ async () => {
-              await axios.delete('/api/issues/' + issueId);
-              router.push('/issues/list');
-              router.refresh();
+              <Button variant="solid" color="red" onClick={async () => {
+              
+                try {
+                  throw Error()
+                  await axios.delete('/api/issues/' + issueId);
+                  router.push('/issues/list');
+                  router.refresh();
+                } catch (error) {
+                  setError(true);
+                }
               }}>Revoke access</Button>
             </AlertDialog.Action>
           </Flex>
-         </AlertDialog.Content>
-     </AlertDialog.Root>
-  )
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content style={{ maxWidth: 500 }}>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>This issue not be deleted</AlertDialog.Description>
+          <Flex mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray"
+              onClick={() => setError(false)}>Cancel</Button>
+            </AlertDialog.Cancel>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
+  );
 };
 
 export default DeleteIssueButton
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
