0675e2cd3d3e44c9de1cf80b26715e682504df49
Md Abdul Quyum <quyumbdse@gmail.com.com>
2024-02-06 12:11:27 +0100

    Improve the UX

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Spinner } from '@/app/components';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
@@ -7,12 +8,25 @@ import { useState } from 'react';
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
+
+  const deleteIssue = async () => {         
+    try {
+      setDeleting(true);
+      await axios.delete('/api/issues/' + issueId);
+      router.push('/issues/list');
+      router.refresh();
+    } catch (error) {
+      setDeleting(false);
+      setError(true);
+    };
+  };
 
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color='red'>Delete Issue</Button>
+          <Button color='red' disabled={isDeleting}>Delete Issue{isDeleting && <Spinner/>}</Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content style={{ maxWidth: 500 }}>
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
@@ -28,17 +42,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button variant="solid" color="red" onClick={async () => {
-              
-                try {
-                  throw Error()
-                  await axios.delete('/api/issues/' + issueId);
-                  router.push('/issues/list');
-                  router.refresh();
-                } catch (error) {
-                  setError(true);
-                }
-              }}>Revoke access</Button>
+              <Button variant="solid" color="red" onClick={deleteIssue}>Delete Issue</Button>
             </AlertDialog.Action>
           </Flex>
         </AlertDialog.Content>
