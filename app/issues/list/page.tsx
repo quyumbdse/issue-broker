357cb7412fe4f036d274166a40c7694959d5a7a3
Md Abdul Quyum <quyumbdse@gmail.com.com>
2024-02-07 16:34:22 +0100

    Make column sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,16 +1,23 @@
 
 import { Table, TableColumnHeaderCell } from '@radix-ui/themes'
 import prisma from '@/prisma/client';
-import React from 'react'
+import NextLink from 'next/link';
 import {IssueStatusBadge, Link} from '@/app/components';
 import IssueActions from './IssueActions';
-import { Status } from '@prisma/client';
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 
 interface Props {
-  searchParams: {status: Status}
+  searchParams: {status: Status, orderBy: keyof Issue}
 }
 
-const IssuesPage = async ({searchParams}: Props) => {
+const IssuesPage = async ({ searchParams }: Props) => {
+  
+  const columns: { label: string; value: keyof Issue; className?: string}[] = [
+    { label: 'Issue', value: 'title' },
+    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+    { label: 'Created', value: 'createdAt',className: 'hidden md:table-cell'}
+  ]
 
   const statases = Object.values(Status);
   const status = statases.includes(searchParams.status)
@@ -32,9 +39,13 @@ const IssuesPage = async ({searchParams}: Props) => {
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
-            <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
-            <TableColumnHeaderCell className='hidden md:table-cell'>Status</TableColumnHeaderCell>
-            <TableColumnHeaderCell className='hidden md:table-cell'>Dated</TableColumnHeaderCell>
+            {columns.map(column => (
+              <TableColumnHeaderCell key={column.value}>
+                <NextLink href={{
+                  query: { ...searchParams, orderBy: column.value }
+                }}>{column.label}</NextLink>
+                {column.value === searchParams.orderBy && <ArrowUpIcon className='inline'/>}
+              </TableColumnHeaderCell>))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
