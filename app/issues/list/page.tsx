836befb7ee848aac9ca51112736631a8a1bc5300
Md Abdul Quyum <quyumbdse@gmail.com.com>
2024-02-08 11:42:35 +0100

    Refactor: extract issue table

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,37 +1,24 @@
 
-import { Table, TableColumnHeaderCell } from '@radix-ui/themes'
+import { Flex } from '@radix-ui/themes'
 import prisma from '@/prisma/client';
-import NextLink from 'next/link';
-import {IssueStatusBadge, Link} from '@/app/components';
 import IssueActions from './IssueActions';
-import { Issue, Status } from '@prisma/client';
-import { ArrowDownIcon, ArrowUpIcon } from '@radix-ui/react-icons';
+import { Status } from '@prisma/client';
 import Pagination from '@/app/components/Pagination';
+import IssueTable, { IssueQuery, columnNames } from './IssueTable';
 
 interface Props {
-  searchParams: {
-    status: Status,
-    orderBy: keyof Issue,
-    page: string
-    sort: 'asc' | 'desc'
-  }
-}
+  searchParams: IssueQuery 
+};
 
 const IssuesPage = async ({ searchParams }: Props) => {
   
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: 'Issue', value: 'title' },
-    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
-    { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' }
-  ]
-
   const statases = Object.values(Status);
   const status = statases.includes(searchParams.status)
     ? searchParams.status
     : undefined
   const where = { status };
-  const orderBy = columns
-    .map(column => column.value)
+
+  const orderBy = columnNames
     .includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: searchParams.sort }
     : undefined;
@@ -45,56 +32,21 @@ const IssuesPage = async ({ searchParams }: Props) => {
     take: pageSize
   });
 
-  const issueCount = await prisma.issue.count({where});
+  const issueCount = await prisma.issue.count({ where });
 
   return (
-    <div className='space-y-5'>
-      <div>
-        <IssueActions />
-      </div>
-    
-      <Table.Root variant='surface'>
-        <Table.Header>
-          <Table.Row>
-            {columns.map(column => (
-              <TableColumnHeaderCell key={column.value} className={column.className}>
-                {column.label}
-                <NextLink href={{
-                            query: { ...searchParams, orderBy: column.value, sort: 'asc' }
-                        }}><ArrowUpIcon className='inline nav-link' /></NextLink>
-
-                        <NextLink href={{
-                            query: { ...searchParams, orderBy: column.value, sort: 'desc' }
-                }}><ArrowDownIcon className='inline nav-link' />
-                </NextLink>
-               
-                {/* <NextLink href={{
-                  query: { ...searchParams, orderBy: column.value }
-                }}>{column.label}</NextLink>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className='inline'/>} */}
-              </TableColumnHeaderCell>))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-            {issues.map(issue => <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>
-                <div className='block md:hidden'><IssueStatusBadge status={issue.status} /></div>
-              </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{ issue.createdAt.toDateString() }</Table.Cell>
-            </Table.Row>)}
-        </Table.Body>
-      </Table.Root>
+    <Flex direction='column' gap='3'>
+      <IssueActions />
+      {issues.length > 0 &&
+        <IssueTable searchParams={searchParams} issues={issues} />
+      }
       <Pagination
         pageSize={pageSize}
         currentPage={page}
-        itemCount={issueCount}/>
-    </div>
-  )
-}
+        itemCount={issueCount} />
+    </Flex>
+  );
+};
 
 export const dynamic = 'force-dynamic';
 
