b9fcdb12b7195cb71f5cf91c889609730d9b4326
Md Abdul Quyum <quyumbdse@gmail.com.com>
2024-02-14 19:25:06 +0100

    fix tuth

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,6 @@
 
 export default async function AdminPage() {
    
-    // const session = await getServerSession(authOptions);
-
-    // console.log(session?.user)
-    // if (!session) {
-    //     redirect('/api/auth/signin?callbackUrl=/admin')
-    // }
-
     return (
         <div>
            <p>Admin Page</p>
