6c22feb8c7857c1ffdb42ff379149ee62fba7fde
Md Abdul Quyum <quyumbdse@gmail.com.com>
2024-02-20 21:57:21 +0100

    google callback

--- app/components/GoogleSignInButton.tsx
+++ app/components/GoogleSignInButton.tsx
@@ -1,12 +1,20 @@
 import { FC, ReactNode } from 'react';
 import { Button } from './ui/button';
 import { signIn } from 'next-auth/react';
+import Spinner from './Spinner';
 
 interface GoogleSignInButtonProps {
   children: ReactNode;
 }
 const GoogleSignInButton: FC<GoogleSignInButtonProps> = ({ children }) => {
-  const loginWithGoogle = () => signIn('google', {callbackUrl:'https://issue-broker.vercel.app'});
+  const loginWithGoogle = async () => {
+    try {
+      <Spinner/>
+      await signIn('google', {callbackUrl:'/'});
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <Button onClick={loginWithGoogle} className='w-full'>
