2592243f4bf3e363f566cfbdb672663d0093a926
Md Abdul Quyum <quyumbdse@gmail.com.com>
2024-02-20 21:46:03 +0100

    callbackurl

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ interface AuthLayoutProps {
 }
 
 const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
-  return <div className='bg-slate-40 p-10 mt-40 rounded-md w-max container mx-auto'>{children}</div>;
+  return <div className='bg-slate-40 p-10 mt-30 rounded-md w-max container mx-auto'>{children}</div>;
 };
 
 export default AuthLayout;
